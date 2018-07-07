43ffb5439f29a63d66bc0b00aa3bc9b680de589b
Administrator <Administrator@WIN-OKH646EVNPT>
2018-07-07 08:45:07 +0800

    value obj

---
+++ src/main/java/com/dml/mpgame/GameValueObject.java
@@ -0,0 +1,48 @@
+package com.dml.mpgame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameValueObject {
+
+	private String id;
+	private GameState state;
+	private List<GamePlayer> players;
+
+	public GameValueObject(Game game) {
+		id = game.getId();
+		state = game.getState();
+		players = new ArrayList<GamePlayer>();
+		game.getIdPlayerMap().values().forEach((player) -> {
+			GamePlayer copy = new GamePlayer();
+			copy.setId(player.getId());
+			copy.setState(player.getState());
+			players.add(copy);
+		});
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public GameState getState() {
+		return state;
+	}
+
+	public void setState(GameState state) {
+		this.state = state;
+	}
+
+	public List<GamePlayer> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<GamePlayer> players) {
+		this.players = players;
+	}
+
+}

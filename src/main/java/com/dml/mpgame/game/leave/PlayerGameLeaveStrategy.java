24abd7b3907e542d593a1710348511faa5b965c9
林少聪 < @PC-20180515PRDG>
2019-02-13 14:37:19 +0800

    离开就解散房间

--- src/main/java/com/dml/mpgame/game/leave/PlayerGameLeaveStrategy.java
+++ src/main/java/com/dml/mpgame/game/leave/PlayerGameLeaveStrategy.java
@@ -4,7 +4,7 @@ import com.dml.mpgame.game.Game;
 import com.dml.mpgame.game.player.GamePlayer;
 
 /**
- * 只要离开就直接退出
+ * 只要离开就直接解散房间
  * 
  * @author lsc
  *
@@ -15,8 +15,8 @@ public class PlayerGameLeaveStrategy implements GameLeaveStrategy {
 	public void leave(String playerId, Game game) throws Exception {
 		GamePlayer player = game.findPlayer(playerId);
 		if (player != null) {
-			// 直接退出
-			game.removePlayer(playerId);
+			// 直接解散房间
+			game.cancel();
 		}
 	}
 

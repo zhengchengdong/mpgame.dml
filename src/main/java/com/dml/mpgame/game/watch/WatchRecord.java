502c40f931a85e80a976bf0bc3322886d3ad3c2d
ethan0353 <yinziyu7@gmail.com>
2019-03-05 10:44:56 +0800

    add watch

--- src/main/java/com/dml/mpgame/game/watch/WatchRecord.java
+++ src/main/java/com/dml/mpgame/game/watch/WatchRecord.java
@@ -1,44 +1,28 @@
 package com.dml.mpgame.game.watch;
 
+import java.util.List;
+
 /**
  * @author yins
  * @Description: 观战记录bean
  */
 public class WatchRecord {
-    private String id; // game server id
-    private String playerId;
-    private long joinTime;
-    private long leaveTime;
+    private String gameId; // game server id
+    private List<Watcher> watchers;
 
-    public String getId() {
-        return id;
+    public String getGameId() {
+        return gameId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
     }
 
-    public String getPlayerId() {
-        return playerId;
+    public List<Watcher> getWatchers() {
+        return watchers;
     }
 
-    public void setPlayerId(String playerId) {
-        this.playerId = playerId;
-    }
-
-    public long getJoinTime() {
-        return joinTime;
-    }
-
-    public void setJoinTime(long joinTime) {
-        this.joinTime = joinTime;
-    }
-
-    public long getLeaveTime() {
-        return leaveTime;
-    }
-
-    public void setLeaveTime(long leaveTime) {
-        this.leaveTime = leaveTime;
+    public void setWatchers(List<Watcher> watchers) {
+        this.watchers = watchers;
     }
 }
